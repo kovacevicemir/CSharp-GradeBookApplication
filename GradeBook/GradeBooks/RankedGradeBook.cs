8b68abba7193dd17d226ded1cb7dd06a7bf08146
kovacevicemir <32427520+kovacevicemir@users.noreply.github.com>
2019-02-17 02:39:41 +1000

    Override RankedGradeBooks GetLetter Grade 3/5 pass

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,54 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base (name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students");
+            }
+            else
+            {
+                //treshold and get list of average grades by descending order,
+                var treshold = (int)Math.Ceiling(Students.Count * 0.2);
+                List<double> grades = new List<double>();
+
+                foreach (Student student in Students)
+                {
+                        grades.Add(student.AverageGrade);
+                }
+
+                grades.OrderByDescending(i => i);
+
+                //Final step
+
+
+                if (grades[treshold - 1] <= averageGrade)
+                {
+                    return 'A';
+                }
+                else if (grades[(treshold * 2) - 1] <= averageGrade)
+                {
+                    return 'B';
+                }
+                else if (grades[(treshold * 3) - 1] <= averageGrade)
+                {
+                    return 'C';
+                }
+                else if (grades[(treshold * 4) - 1] <= averageGrade)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
+
+            }
+
+            return base.GetLetterGrade(averageGrade);
         }
     }
 }
